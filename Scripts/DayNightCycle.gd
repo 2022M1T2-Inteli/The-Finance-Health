4463493efc2b6e0e081f31459a67a3abb5219695
Leandro Duarte <leandro@Leandro.Leandro>
2022-04-14 19:52:44 -0300

    Teste-2

--- Scripts/DayNightCycle.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasModulate
-
-func _process(delta):
-	var CurrentFrame = range_lerp(Global.horas,0,24,0,24)
-	$AnimationPlayer.play("DayNightCycle")
-	$AnimationPlayer.seek(CurrentFrame)
-
-
--- Scripts/tijolos.gd
+++
@@ -1,5 +0,0 @@
-extends StaticBody2D
-
-func hit():
-	get_parent().queue_free() 
-	Global.tijolos += 1 
